3ef231cc51f7234f89c14281b2b28d7e71bc8563
Kryptikko <velin.br.vangelov@gmail.com>
2020-02-02 01:06:22 +0200

    Shifts View

--- src/views.py
+++ src/views.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
-from marshmallow import Schema, fields
 
 from src.controllers import shift_controller
 from src.entities.GetShiftsResponse import GetShiftsResponse
+from src.lib.sqlalchemyencoder import encode
 
 app = Flask(__name__)
 
@@ -10,16 +11,8 @@ app = Flask(__name__)
 @app.route("/shifts")
 def get_shifts():
     shifts = shift_controller.get_all()
-    def _format_output(shift):
-        return dict(shift)
-        # return shift.__dict__
-        # return shift.to_dict()
-        # return jsonify(shift)
-    # response = [dict(r) for r in shifts]
-    response = map(_format_output, shifts)
-    print(response)
-    # return response
-    return jsonify({"response": response})
+    response = map(encode, shifts)
+    return jsonify(response)
 
 @app.route("/shifts/:id/contracts")
 def get_shift_contracts(shift_id):
