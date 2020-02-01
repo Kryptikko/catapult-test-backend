d3582cf3bb9149e03898347641e168838aa7bc8d
Kryptikko <velin.br.vangelov@gmail.com>
2020-02-02 01:12:31 +0200

    Cleanup

--- src/controllers.py
+++ src/controllers.py
@@ -1,20 +1,14 @@
-from src.models import sess, Base, engine
+from src.models import sess
 from src.models.Shifts import Shift
 from src.models.Contractors import Contractor
-import json
 
-# Base.metadata.create_all(engine)
-# sess.commit()
-#
-# api = ""
-# with open("api.json", "r") as api_file:
-#     api = json.load(api_file)
 
 class ShiftController:
+
     def get_all(self):
         return sess.query(Shift).all()
 
-    def get_shift_contractors(shift_id):
+    def get_shift_contractors(self, shift_id):
         pass
 
 shift_controller = ShiftController()
--- src/views.py
+++ src/views.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify
-
 from src.controllers import shift_controller
 from src.entities.GetShiftsResponse import GetShiftsResponse
 from src.lib.sqlalchemyencoder import encode
@@ -13,6 +12,7 @@ def get_shifts():
     shifts = shift_controller.get_all()
     response = map(encode, shifts)
     return jsonify(response)
+
 
 @app.route("/shifts/:id/contracts")
 def get_shift_contracts(shift_id):
